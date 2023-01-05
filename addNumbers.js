e50c9c56b4e2fb2e9c2e7c14b84b02920dd625e5
Daniel Lee <dlee13195@gmail.com>
2023-01-05 16:47:47 -0500

    addNumber

---
+++ addNumbers.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if numsLeft > 0 {
+    reader.question( " Put in a number: ", )
+    }
+}
+
+
+
+
+
+
