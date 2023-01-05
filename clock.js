c2cc60bd8fe9919fcfc0ed778797d83ff8997148
Daniel Lee <dlee13195@gmail.com>
2023-01-05 15:14:26 -0500

    constructor

---
+++ clock.js
@@ -0,0 +1,25 @@
+class Clock {
+    constructor() {
+        const timeNow = new Date () 
+
+        this.hours = timeNow.prototype.getHours() 
+        this.minutes = timeNow.prototype.getMinutes()
+        this.seconds = timeNow.prototype.getSeconds()
+
+        printTime() 
+
+        setTimeout
+    }
+
+        printTime() {
+            
+
+    }
+
+    _tick() {
+
+    }
+
+}
+
+const clock = new Clock();
